3d0869e0a0454e579ae4f58944a7312af6e4ae42
k2gaurav <kumargaurav22892@gmail.com>
2024-05-04 18:09:44 +0530

    Factory design pattern

--- creational_design_patterns/factory.py
+++ creational_design_patterns/factory.py
@@ -74,10 +74,10 @@ class DeluxCheeseBurger(Burger):
     def serve(self):
         pass
 
-class VeganBurger(Burger):
+class DeluxVeganBurger(Burger):
     def __int__(self):
         super().__int__()
-        self.name = "Vegan Burger"
+        self.name = "Delux Vegan Burger"
 
     def prepare(self):
         pass
@@ -94,7 +94,40 @@ class BurgerStore(ABC):
     def create_burger(self, item: Burgers) -> Burger:
         pass
 
-    def order_type(self,type:Burgers) -> Burger:
+    def order_burger(self,type:Burgers) -> Burger:
         burger = self.create_burger(type)
         print(f"---Making a {burger.get_name()}")
+        burger.prepare()
+        burger.cook()
+        burger.serve()
+        return burger
 
+class CheeseBurgerStore(BurgerStore):
+
+    def create_burger(self, item: Burgers) -> Burger:
+        if item == Burgers.CHEESE:
+            return CheeseBurger()
+        elif item == Burgers.DELUXECHEESE:
+            return DeluxCheeseBurger()
+        else:
+            return None
+
+
+class VeganBurgerStore(BurgerStore):
+
+    def create_burger(self, item: Burgers) -> Burger:
+        if item == Burgers.VEGAN:
+            return VeganBurger()
+        elif item == Burgers.DELUXEVEGAN:
+            return DeluxVeganBurger()
+        else:
+            return None
+
+
+cheese_store = CheeseBurgerStore()
+vegan_store = VeganBurgerStore()
+
+burger = cheese_store.order_burger(Burgers.CHEESE)
+print(f"Ethan ordered a {burger.get_name()}")
+burger = vegan_store.order_burger(Burgers.VEGAN)
+print(f"Joel ordered a {burger.get_name()}")
